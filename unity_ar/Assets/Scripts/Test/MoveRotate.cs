6d4ca310405647f9233e9f74086711d79ada5163
水野淳 <>
2021-03-30 15:31:04 +0900

    動作検証用のテストシーンを作成

---
+++ unity_ar/Assets/Scripts/Test/MoveRotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveRotate : MonoBehaviour
+{
+    [SerializeField]
+    public bool isRotate = true;
+
+    [SerializeField]
+    public float rotateSpeed = 30.0f;
+
+    public void Update()
+    {
+        if (!isRotate)
+        {
+            return;
+        }
+
+        var angles = this.transform.rotation.eulerAngles;
+        angles.y += rotateSpeed * Time.deltaTime;
+        this.transform.rotation = Quaternion.Euler(angles);
+    }
+}
