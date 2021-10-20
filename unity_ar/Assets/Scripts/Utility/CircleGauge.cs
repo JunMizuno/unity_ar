7157ec68f6457914f47d3db51afc0fee3c199170
水野淳 <>
2021-10-20 22:54:02 +0900

    サークル型のゲージを作成

---
+++ unity_ar/Assets/Scripts/Utility/CircleGauge.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleGauge : MonoBehaviour
+{
+    [SerializeField]
+    public Image gauge;
+
+    [SerializeField, Range(0.0f, 1.0f)]
+    public float fillValue = 0.0f;
+
+    public void Update()
+    {
+        if (gauge != null)
+        {
+            gauge.fillAmount = fillValue;
+        }
+    }
+}
