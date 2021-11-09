a36ead691d0f49e0226578d6c006c5d677a7b9e3
水野淳 <>
2021-11-09 22:35:10 +0900

    周期運動のスクリプトを追加

---
+++ unity_ar/Assets/Scripts/Test/MoveFloat.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveFloat : MonoBehaviour
+{
+    [SerializeField]
+    public bool isFloat = true;
+
+    [SerializeField]
+    public float floatDistance = 5.0f;
+
+    private void Update()
+    {
+        if (!isFloat)
+        {
+            return;
+        }
+
+        var pos = this.transform.localPosition;
+        pos.y = floatDistance * Mathf.Sin(Time.time);
+        this.transform.localPosition = pos;
+    }
+}
