b4053d2f867bdffe6b8e0f41442f79aa0db957e2
水野淳 <>
2021-11-10 18:55:11 +0900

    ポストエフェクト関連を追加

---
+++ unity_ar/Assets/Scripts/PostEffect/CameraFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraFilter : MonoBehaviour
+{
+    [SerializeField]
+    private Material filter;
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+		if (filter == null)
+		{
+			Graphics.Blit(source, destination);
+		}
+		else
+		{
+			Graphics.Blit(source, destination, filter);
+		}
+	}
+}
