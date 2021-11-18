c06a2823161d0aa1c7a1f2b8f0bca84b96dc134a
水野淳 <>
2021-11-18 23:30:45 +0900

    簡素なビルボード処理を追加

---
+++ unity_ar/Assets/Scripts/Utility/BillBoard.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillBoard : MonoBehaviour
+{
+    private void Update()
+    {
+        var pos = Camera.main.transform.position;
+        pos.y = this.gameObject.transform.position.y;
+        this.gameObject.transform.LookAt(pos);
+    }
+}
