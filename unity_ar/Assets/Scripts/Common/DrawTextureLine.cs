c64ef8239f78a57ad64eb20d027d0de6f81acf87
水野淳 <>
2021-12-10 14:27:24 +0900

    Blenderからモデルをインポートするところまでを操作

---
+++ unity_ar/Assets/Scripts/Common/DrawTextureLine.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DrawTextureLine : MonoBehaviour
+{
+    [SerializeField]
+    public Material material;
+
+    [SerializeField]
+    public float lineSize;
+
+
+    public void Update()
+    {
+        
+    }
+
+    public void CreateMesh()
+    {
+
+    }
+
+    public void DownCursor(Vector3 touchPoint)
+    {
+
+    }
+
+    public void MoveCursor(Vector3 touchPoint)
+    {
+
+    }
+}
