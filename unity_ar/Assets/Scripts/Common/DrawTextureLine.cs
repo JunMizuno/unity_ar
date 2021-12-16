661f92ece8ce361dbe345d2004121985ffbb47f5
水野淳 <>
2021-12-16 11:39:45 +0900

    シェーダーのLODのテスト

--- unity_ar/Assets/Scripts/Common/DrawTextureLine.cs
+++ unity_ar/Assets/Scripts/Common/DrawTextureLine.cs
@@ -8,12 +8,32 @@ public class DrawTextureLine : MonoBehaviour
     public Material material;
 
     [SerializeField]
-    public float lineSize;
+    public float lineSize = 0.6f;
 
+    [SerializeField]
+    public float scrollSpeed = 0.18f;
+
+    private int indexOffset = 0;
+    private float uvOffset = 0.0f;
+
+    private List<Vector3> pointList = new List<Vector3>();
+    private List<Vector3> verticsList = new List<Vector3>();
+    private List<Vector2> uvList = new List<Vector2>();
+    private List<int> trisList = new List<int>();
 
     public void Update()
     {
-        
+        // ファーストタップ
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 touchPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+        // タップムーブ
+        else if (Input.GetMouseButton(0))
+        {
+            Vector3 touchPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //PenMove(tp, this.penSize);
+        }
     }
 
     public void CreateMesh()
---
+++ unity_ar/Assets/Scripts/Test/TestLOD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLOD : MonoBehaviour
+{
+    public void Start()
+    {
+        var meshRenderer = GetComponent<MeshRenderer>();
+        if (meshRenderer != null)
+        {
+            // @memo.mizuno シェーダー側のLOD値は降順で設定しないといけない。
+            meshRenderer.material.shader.maximumLOD = 60;
+        }
+    }
+}
